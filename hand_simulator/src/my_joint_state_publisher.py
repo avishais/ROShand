ab83f114873b6a5883584635cdfb39f567ec28a5
avishais <avishai.sintov@gmail.com>
2018-10-14 23:56:02 -0400

    T42 urdf and controller

--- hand_simulator/src/my_joint_state_publisher.py
+++ hand_simulator/src/my_joint_state_publisher.py
@@ -10,7 +10,7 @@ import PyKDL
 
 class my_joint_state_publisher():
 
-    joint_angles = [0.,0.,0.,0.,0.,0.,0.,0.]#, dtype=np.float32)
+    joint_angles = None#, dtype=np.float32)
     order = np.array([0,0,0,0,0,0,0,0,0])
     msg = Float32MultiArray()
     Gtype = None
@@ -18,8 +18,14 @@ class my_joint_state_publisher():
     def __init__(self):
         rospy.init_node('my_joint_state_publisher', anonymous=True)
 
-        if rospy.has_param('~gripper/gripper_type'):
-            self.Gtype = rospy.get_param('~gripper/gripper_type')
+        # if rospy.has_param('~gripper/gripper_type'):
+            # self.Gtype = rospy.get_param('~gripper/gripper_type')
+        self.Gtype = 'model_T42'
+
+        if self.Gtype == 'reflex' or self.Gtype == 'model_O':
+            self.joint_angles = [0.,0.,0.,0.,0.,0.,0.,0.]
+        else:
+            self.joint_angles = [0.,0.,0.,0.]
 
         rospy.Subscriber('/gazebo/link_states', LinkStates, self.linkStatesCallback)
         joint_states_pub = rospy.Publisher('/hand/my_joint_states', Float32MultiArray, queue_size=10)
@@ -39,14 +45,21 @@ class my_joint_state_publisher():
 
         q_world = PyKDL.Rotation.Quaternion(msg.pose[0].orientation.x, msg.pose[0].orientation.y, msg.pose[0].orientation.z, msg.pose[0].orientation.w)
         q_base = PyKDL.Rotation.Quaternion(msg.pose[self.order[0]].orientation.x, msg.pose[self.order[0]].orientation.y, msg.pose[self.order[0]].orientation.z, msg.pose[self.order[0]].orientation.w)
-        q_1_1 = PyKDL.Rotation.Quaternion(msg.pose[self.order[1]].orientation.x, msg.pose[self.order[1]].orientation.y, msg.pose[self.order[1]].orientation.z, msg.pose[self.order[1]].orientation.w)
-        q_1_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[2]].orientation.x, msg.pose[self.order[2]].orientation.y, msg.pose[self.order[2]].orientation.z, msg.pose[self.order[2]].orientation.w)
-        q_1_3 = PyKDL.Rotation.Quaternion(msg.pose[self.order[3]].orientation.x, msg.pose[self.order[3]].orientation.y, msg.pose[self.order[3]].orientation.z, msg.pose[self.order[3]].orientation.w)
-        q_2_1 = PyKDL.Rotation.Quaternion(msg.pose[self.order[4]].orientation.x, msg.pose[self.order[4]].orientation.y, msg.pose[self.order[4]].orientation.z, msg.pose[self.order[4]].orientation.w)
-        q_2_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[5]].orientation.x, msg.pose[self.order[5]].orientation.y, msg.pose[self.order[5]].orientation.z, msg.pose[self.order[5]].orientation.w)
-        q_2_3 = PyKDL.Rotation.Quaternion(msg.pose[self.order[6]].orientation.x, msg.pose[self.order[6]].orientation.y, msg.pose[self.order[6]].orientation.z, msg.pose[self.order[6]].orientation.w)
-        q_3_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[7]].orientation.x, msg.pose[self.order[7]].orientation.y, msg.pose[self.order[7]].orientation.z, msg.pose[self.order[7]].orientation.w)
-        q_3_3 = PyKDL.Rotation.Quaternion(msg.pose[self.order[8]].orientation.x, msg.pose[self.order[8]].orientation.y, msg.pose[self.order[8]].orientation.z, msg.pose[self.order[8]].orientation.w)
+        if self.Gtype == 'reflex' or self.Gtype == 'model_O':
+            q_1_1 = PyKDL.Rotation.Quaternion(msg.pose[self.order[1]].orientation.x, msg.pose[self.order[1]].orientation.y, msg.pose[self.order[1]].orientation.z, msg.pose[self.order[1]].orientation.w)
+            q_1_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[2]].orientation.x, msg.pose[self.order[2]].orientation.y, msg.pose[self.order[2]].orientation.z, msg.pose[self.order[2]].orientation.w)
+            q_1_3 = PyKDL.Rotation.Quaternion(msg.pose[self.order[3]].orientation.x, msg.pose[self.order[3]].orientation.y, msg.pose[self.order[3]].orientation.z, msg.pose[self.order[3]].orientation.w)
+            q_2_1 = PyKDL.Rotation.Quaternion(msg.pose[self.order[4]].orientation.x, msg.pose[self.order[4]].orientation.y, msg.pose[self.order[4]].orientation.z, msg.pose[self.order[4]].orientation.w)
+            q_2_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[5]].orientation.x, msg.pose[self.order[5]].orientation.y, msg.pose[self.order[5]].orientation.z, msg.pose[self.order[5]].orientation.w)
+            q_2_3 = PyKDL.Rotation.Quaternion(msg.pose[self.order[6]].orientation.x, msg.pose[self.order[6]].orientation.y, msg.pose[self.order[6]].orientation.z, msg.pose[self.order[6]].orientation.w)
+            q_3_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[7]].orientation.x, msg.pose[self.order[7]].orientation.y, msg.pose[self.order[7]].orientation.z, msg.pose[self.order[7]].orientation.w)
+            q_3_3 = PyKDL.Rotation.Quaternion(msg.pose[self.order[8]].orientation.x, msg.pose[self.order[8]].orientation.y, msg.pose[self.order[8]].orientation.z, msg.pose[self.order[8]].orientation.w)
+        else:
+            q_1_1 = PyKDL.Rotation.Quaternion(msg.pose[self.order[1]].orientation.x, msg.pose[self.order[1]].orientation.y, msg.pose[self.order[1]].orientation.z, msg.pose[self.order[1]].orientation.w)
+            q_1_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[2]].orientation.x, msg.pose[self.order[2]].orientation.y, msg.pose[self.order[2]].orientation.z, msg.pose[self.order[2]].orientation.w)
+            q_2_1 = PyKDL.Rotation.Quaternion(msg.pose[self.order[4]].orientation.x, msg.pose[self.order[4]].orientation.y, msg.pose[self.order[4]].orientation.z, msg.pose[self.order[4]].orientation.w)
+            q_2_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[5]].orientation.x, msg.pose[self.order[5]].orientation.y, msg.pose[self.order[5]].orientation.z, msg.pose[self.order[5]].orientation.w)
+            
         
         if self.Gtype == 'reflex':
             # self.joint_angles = [f1_1,f1_2,f1_3,f2_1,f2_2,f2_3,f3_2,f3_3]
@@ -56,7 +69,7 @@ class my_joint_state_publisher():
             self.joint_angles[3] = (q_world*q_2_1.Inverse()).GetEulerZYX()[0]
             self.joint_angles[4] = (q_2_2.Inverse()*q_2_1).GetEulerZYX()[1]+0.2807829   # There is an initial offset of 0.2807829 rad
             self.joint_angles[5] = (q_2_3.Inverse()*q_2_2).GetEulerZYX()[1] 
-            self.joint_angles[6] = -((q_3_2.Inverse()).GetEulerZYX()[1]-np.pi/2)+0.2807829   # There is an initial offset of 0.2807829 rad
+            self.joint_angles[6] = -((q_world*q_3_2.Inverse()).GetEulerZYX()[1]-np.pi/2)+0.2807829   # There is an initial offset of 0.2807829 rad
             self.joint_angles[7] = (q_3_3.Inverse()*q_3_2).GetEulerZYX()[1]
 
         if self.Gtype == 'model_O':
@@ -67,8 +80,23 @@ class my_joint_state_publisher():
             self.joint_angles[3] = -(q_world*q_2_1.Inverse()).GetEulerZYX()[2]-np.pi/2
             self.joint_angles[4] = -(q_2_2.Inverse()*q_2_1).GetEulerZYX()[0]
             self.joint_angles[5] = -(q_2_3.Inverse()*q_2_2).GetEulerZYX()[0] 
-            self.joint_angles[6] = -(q_3_2.Inverse()).GetEulerZYX()[0]
+            self.joint_angles[6] = -(q_world*q_3_2.Inverse()).GetEulerZYX()[0]
             self.joint_angles[7] = -(q_3_3.Inverse()*q_3_2).GetEulerZYX()[0]
+
+        if self.Gtype == 'model_T42':
+            a = (q_1_1.Inverse()*q_base).GetEulerZYX()
+            if a[0] < 0:
+                self.joint_angles[0] = -a[1]
+            else:
+                self.joint_angles[0] = np.pi+a[1]
+            self.joint_angles[1] = -(q_1_2.Inverse()*q_1_1).GetEulerZYX()[2]
+            a = (q_2_1.Inverse()*q_base).GetEulerZYX()
+            if a[0] < 0:
+                self.joint_angles[2] = -a[1]
+            else:
+                self.joint_angles[2] = np.pi+a[1]
+            self.joint_angles[3] = -(q_2_2.Inverse()*q_2_1).GetEulerZYX()[2]
+
 
     def getNameOrder(self, names):
 
